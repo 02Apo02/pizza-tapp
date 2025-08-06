f729a9113e7f192aa6876846ac6e633e91283238
02Apo02 <xzaza484@gmail.com>
2025-08-06 10:27:43 +0300

    Initial working version

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Pizza Tapp Oyunu</h1>
+      <p>Oyun yakında burada başlayacak!</p>
+    </div>
+  );
+}
+
+export default App;
