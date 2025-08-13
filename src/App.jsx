cd2dc576f25859bd2cfc7ef17d2dacdd7629b6e2
02Apo02 <119961341+02Apo02@users.noreply.github.com>
2025-08-13 09:00:15 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import KartlarEkrani from "./KartlarEkrani";
+import KartlarEkrani from "./kartlarekrani";
 import LoadingScreen from "./LoadingScreen";
 
 export default function App() {
