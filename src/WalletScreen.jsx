a48e843cbc1d404c132d1bd514be85d09ea8973b
02Apo02 <119961341+02Apo02@users.noreply.github.com>
2025-08-16 20:19:03 +0300

    Create WalletScreen.jsx

---
+++ src/WalletScreen.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function WalletScreen() {
+  return (
+    <div className="screen">
+      <h2>👛 Cüzdan</h2>
+      <p>Bakiyeniz: 0 PZT</p>
+    </div>
+  );
+}
