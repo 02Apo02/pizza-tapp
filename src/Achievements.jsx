385c3d11ade44944e45abfe022781344b114e35e
02Apo02 <119961341+02Apo02@users.noreply.github.com>
2025-08-17 11:42:24 +0300

    Create Achievements.jsx

---
+++ src/Achievements.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Achievements(){
+  return (
+    <div className="screen">
+      <div className="title">🏆 Başarımlar</div>
+      <div className="card">İlk 100 tap ✔</div>
+      <div className="card" style={{marginTop:10}}>Cüzdan bağlandı ✔</div>
+    </div>
+  );
+}
