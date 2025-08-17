7030eb282d9298cf6a874a4f665cf038f9d689c0
02Apo02 <119961341+02Apo02@users.noreply.github.com>
2025-08-17 11:43:54 +0300

    Create Tasks.jsx

---
+++ src/Tasks.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Tasks(){
+  return (
+    <div className="screen">
+      <div className="title">📋 Görevler</div>
+      <div className="card">Telegram kanalına katıl</div>
+      <div className="card" style={{marginTop:10}}>Günlük giriş yap</div>
+    </div>
+  );
+}
