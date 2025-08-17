227d8b75eded9c9a7d732ae6a28c1b1c47cae6e4
02Apo02 <119961341+02Apo02@users.noreply.github.com>
2025-08-17 11:43:30 +0300

    Create Airdrop.jsx

---
+++ src/Airdrop.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Airdrop(){
+  return (
+    <div className="screen">
+      <div className="title">🎁 Airdrop</div>
+      <div className="card">Görevleri tamamla, snapshot tarihinde ödül al.</div>
+    </div>
+  );
+}
